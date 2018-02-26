ab1585d42b2422d7338a7ab0709e888ad80e92ae
bakbak85 <bakbak85@tamu.edu>
2018-02-26 09:16:59 -0600

    Got HTTP Requests working
    send a message to the server working

--- Test/app/src/main/java/com/ergonator/test/MainActivity.java
+++ Test/app/src/main/java/com/ergonator/test/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,7 +14,18 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -24,6 +36,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView textView_X;
     private TextView textView_Y;
     private TextView textView_Z;
+    private Button serverButton;
+    private RequestQueue requestQueue;
+    private String url = "http://10.231.227.151:3000/data";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +59,48 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         textView_X = (TextView)findViewById(R.id.textView_X);
         textView_Y = (TextView)findViewById(R.id.textView_Y);
         textView_Z = (TextView)findViewById(R.id.textView_Z);
+        serverButton = (Button)findViewById(R.id.serverButton);
+        requestQueue = Volley.newRequestQueue(this);
+
+        serverButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+
+
+                StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>()
+                        {
+                            @Override
+                            public void onResponse(String response) {
+                                // response
+                                Log.d("Response", response);
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // error
+                                Log.d("Error.Response", error.toString());
+                            }
+                        }
+                ) {
+                    @Override
+                    protected Map<String, String> getParams()
+                    {
+                        Map<String, String>  params = new HashMap<String, String>();
+                        params.put("name", "Bradley");
+                        params.put("message", "Hi Ryan");
+
+                        return params;
+                    }
+                };
+                requestQueue.add(postRequest);
+
+
+
+            }
+        });
 
         mSensorManager = (SensorManager) getSystemService(this.SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
