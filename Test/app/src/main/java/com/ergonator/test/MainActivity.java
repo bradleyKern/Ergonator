65d56c771df2705ce505e16d39bb43c8b71759a2
bakbak85 <bakbak85@tamu.edu>
2018-02-26 10:34:04 -0600

    Correctly gathering data from all sensors

--- Test/app/src/main/java/com/ergonator/test/MainActivity.java
+++ Test/app/src/main/java/com/ergonator/test/MainActivity.java
@@ -29,8 +29,11 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    private Sensor mSensor;
+    private Sensor mSensorAccel;
+    private Sensor mSensorGyro;
+    private Sensor mSensorLinAccel;
     private SensorManager mSensorManager;
+    private SensorEventListener mSensorEventListener;
 
     // globally
     private TextView textView_X;
@@ -39,6 +42,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Button serverButton;
     private RequestQueue requestQueue;
     private String url = "http://10.231.227.151:3000/data";
+
+    //All Sensor Data Values
+    private float accelX = 0;
+    private float accelY = 0;
+    private float accelZ = 0;
+    private float gyroX = 0;
+    private float gyroY = 0;
+    private float gyroZ = 0;
+    private float linAccelX = 0;
+    private float linAccelY = 0;
+    private float linAccelZ = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,8 +79,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         serverButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
-
-
+                Log.d("AXVALUE", Float.toString(accelX));
+                Log.d("AYVALUE", Float.toString(accelY));
+                Log.d("AZVALUE", Float.toString(accelZ));
+                Log.d("GXVALUE", Float.toString(gyroX));
+                Log.d("GYVALUE", Float.toString(gyroY));
+                Log.d("GZVALUE", Float.toString(gyroZ));
+                Log.d("LAXVALUE", Float.toString(linAccelX));
+                Log.d("LAYVALUE", Float.toString(linAccelY));
+                Log.d("LAZVALUE", Float.toString(linAccelZ));
+                /*
                 StringRequest postRequest = new StringRequest(Request.Method.POST, url,
                         new Response.Listener<String>()
                         {
@@ -89,21 +111,29 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     protected Map<String, String> getParams()
                     {
                         Map<String, String>  params = new HashMap<String, String>();
-                        params.put("name", "Bradley");
-                        params.put("message", "Hi Ryan");
+                        params.put("User", "Bradley");
+                        params.put("MessageType", "Hi Ryan");
+                        params.put("AccelX", Float.toString(accelX));
+                        params.put("AccelY", Float.toString(accelY));
+                        params.put("AccelZ", Float.toString(accelZ));
+                        params.put("GyroX", Float.toString(gyroX));
+                        params.put("GyroY", Float.toString(gyroY));
+                        params.put("GyroZ", Float.toString(gyroZ));
+                        params.put("LinAccelX", Float.toString(linAccelX));
+                        params.put("LinAccelY", Float.toString(linAccelY));
+                        params.put("LinAccelZ", Float.toString(linAccelZ));
 
                         return params;
                     }
                 };
-                requestQueue.add(postRequest);
-
-
-
+                requestQueue.add(postRequest);*/
             }
         });
 
         mSensorManager = (SensorManager) getSystemService(this.SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorAccel = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorGyro = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mSensorLinAccel = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
     }
 
     @Override
@@ -137,20 +167,38 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public final void onSensorChanged(SensorEvent event) {
         // The light sensor returns a single value.
         // Many sensors return 3 values, one for each axis.
-        float j = event.values[0];
+       /* float j = event.values[0];
         float k = event.values[1];
         float l = event.values[2];
         // Do something with this sensor value.
 
         textView_X.setText("X: " + j + "");
         textView_Y.setText("Y: " + k + "");
-        textView_Z.setText("Z: " + l + "");
+        textView_Z.setText("Z: " + l + "");*/
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            accelX = event.values[0];
+            accelY = event.values[1];
+            accelZ = event.values[2];
+        }
+        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            gyroX = event.values[0];
+            gyroY = event.values[1];
+            gyroZ = event.values[2];
+        }
+        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            linAccelX = event.values[0];
+            linAccelY = event.values[1];
+            linAccelZ = event.values[2];
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mSensorAccel, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mSensorGyro, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mSensorLinAccel, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
